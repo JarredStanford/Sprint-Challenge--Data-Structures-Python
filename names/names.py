0653eb9592e34d9268949cdfbb589473aab4e893
Jarred Stanford <stanj89@gmail.com>
2019-10-18 12:11:50 -0400

    mvp.

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -11,12 +12,19 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+#for name_1 in names_1:
+#        if name_1 in names_2:
+#            duplicates.append(name_1)
+
+binary = BinarySearchTree('')
+for name in names_1:
+    binary.insert(name)
+for name in names_2:
+    if binary.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
+# The original runtime complexity is O(n^2) because it loops through the name_1 list n times (length of name_2) for each n.
--- reverse/reverse.py
+++ reverse/reverse.py
@@ -44,4 +44,12 @@ class LinkedList:
 
   def reverse_list(self):
     # TO BE COMPLETED
-    pass
+    current = self.head
+    prev = None
+    next = None
+    while current:
+      next = current.next_node
+      current.next_node = prev
+      prev = current
+      current = next
+    self.head = prev
